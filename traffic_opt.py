d8dfd48dbfef459b75f0cee523fc359830c343c8
Mohan Singh <7355184966mohan@gmail.com>
2026-04-08 22:21:45 +0000

    built the 1st graphical model

---
+++ traffic_opt.py
@@ -0,0 +1,17 @@
+import networkx as nx 
+import matplotlib.pyplot as plt 
+
+
+
+nodes = ["A","B","C","D"]
+edges = [("A","B"),("B","C"),("C","D"),("A","D"),("B","D")]
+
+G = nx.DiGraph()
+G.add_nodes_from(nodes)
+G.add_edges_from(edges)
+
+pos = nx.spring_layout(G)
+
+nx.draw(G , pos , with_labels = True ,node_size = 2000 , arrows = True )
+
+plt.savefig("graph.png")
