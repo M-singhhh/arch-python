049328f465be8af315fce8fc69f13589c4d463db
Mohan Singh <7355184966mohan@gmail.com>
2026-04-13 21:54:06 +0000

    commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # arch-python
+
+well in this repo i am learning about how i am gonna combine my maths and computional skill to work together to solve real world problems 
+
+Genral flow i have learned to solve a real world problem :  
+1.draw  the static real world system by forming models depecting the same situtation as the real world 
+ex: as i depicted roads with the help of edges of nodular graph and given number to it to depict roads density flow 
+
+2. now make the system dynamic using mathmatics constraits that we follow in the real life like use pdes to depict the flow of the traffic or can talk about linear transformation when talking about robotic hand movements and more 
+
+we did so by understanding the constraints of real world which were q(0) & q(1) = 0 for assuming the flow equation 
+
+we can also assume a powerful genral equation like q = p^a*(1-p)^b 
+this equation can change on the basis of how the behivour our system is like broad road and thin road will change the steepness and peak loacation adn skew is also variable in this system which will really depend on the system we are providing to this equation while the one which we have used is only depend on desity ATM
+
+
+3.
--- traffic_opt.py
+++ traffic_opt.py
@@ -1,10 +1,14 @@
 import networkx as nx 
 import matplotlib.pyplot as plt 
-
+import random
 
 
 nodes = ["A","B","C","D"]
 edges = [("A","B"),("B","C"),("C","D"),("A","D"),("B","D")]
+
+# I used networkx lib to create a graph of nodes and edges which we will se as path of roads here is 
+# networkx doc  -> https://networkx.org/documentation/stable/reference/introduction.html#networkx-basics
+
 
 G = nx.DiGraph()
 G.add_nodes_from(nodes)
@@ -12,7 +16,29 @@ G.add_edges_from(edges)
 
 pos = nx.spring_layout(G)
 
-nx.draw(G , pos , with_labels = True ,node_size = 2000 , arrows = True )
+#now we are adding density to edges that will represent the flow of traffic 
+for u,v in edges:
+    G.add_edge(u,v,density=0.0)
 
+for u,v in G.edges():
+    G[u][v]['density'] = random.uniform(0,1)
+
+#Making the density added visible to the graph  
+edge_lables = {
+    (u,v) : f"{G[u][v]['density']:.2f}"
+for u,v in G.edges()
+}
+
+#drawing graph 
+weights = [G[u][v]['density']*5 for u,v in G.edges()] # this will decide the width of the edge if there is more traffic density then edge width will be thick otherwise thin 
+nx.draw(G , pos , with_labels = True ,node_size = 2000 ,width = weights, arrows = True )
+nx.draw_networkx_edge_labels(G,pos, edge_labels = edge_lables)
 plt.savefig("graph.png") 
+
+def flow(d):
+    return d*(1-d) #this will return the the flow while density is the parameter it takes 
+#how it works ? --> flow = how many car exist * how fast are they moving = density * velocity 
+# and as velocity inversely prop density so we replace velocity with function of density 
+#  why we took only this func d*(d-1) cause at medium density flow will be the best which this function staisfies 
+# we can take any function into account which follow this constraints q (0) = 0 & q(1) = 0 cause no cars no flow and jam no flow either 
 
